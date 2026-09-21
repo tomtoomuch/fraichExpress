5c53c58254454a20d1e0233aa6e9c3d00916a1db
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-09-21 15:00:26 +0200

    <fix>debug1

--- debug1.py
+++ debug1.py
@@ -1,4 +1,17 @@
 import pandas as pd
-df = pd.read_csv("./data/commandes_2025.csv", sep=";", encoding="latin-1")
-print(df.shape)
-print(df["quantite"].sum())
+# on ajoute un bloc try/except pour gérer les erreurs potentielles lors de la lecture du fichier CSV
+try:
+    df = pd.read_csv("./data/commandes2025.csv", sep=";", encoding="latin-1")
+    # 1. Une erreur dans le nom de fichier (underscore entre commandes et 2025 empêchait le script de trouver le fichier
+    #
+    # 2. Le séparateur dans le fichier CSV est un 'point virgule', or la fonction read_csv() utilise le séparateur 'virgule' par défaut.
+    # Il était donc nécessaire de préciser le séparateur en paramètre de la fonction.
+    #
+    # 3. Le fichier CSV est encodé en latin-1.
+    # Il était donc également indispensable de préciser l'encodage du fichier dans les paramètres de la fonction.
+    print(df.shape)
+    print(df["quantite"].sum())
+except FileNotFoundError:
+    print("Fichier non trouvé.")
+except Exception as error:
+    print(f"Une erreur s'est produite : {error}")
