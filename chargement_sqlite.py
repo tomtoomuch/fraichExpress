6842f15eb8e386fcec5ed66c5c0f1c08a9d6ccce
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-09-22 10:46:03 +0200

    <feat>Vérif des doublons ajoutée

--- chargement_sqlite.py
+++ chargement_sqlite.py
@@ -5,10 +5,17 @@
 from pathlib import Path
 import pandas as pd
 import sqlite3 as sqlite
+import logging
 
+# Déclaration des variables globales
 RACINE = Path("./")
 DOSSIER_SOURCES = Path("./data/sources/")
 CHEMIN_BDD = ""
+
+# Initialisation de la journalisation
+
+
+
 
 def charger_sqlite():
     # Recherche du fichier "source_catalogue.db"
@@ -40,20 +47,29 @@ def charger_sqlite():
             for requete in requetes:
                 print(f"{requete["code"]} : {requete["req"]}")
                 df = pd.read_sql_query(requete["req"], connexion)
+                print("La récupération des données est terminées.")
+                print("Nous vérifions la présence de ddoublons dans les données")
+                doublons = df.duplicated()
+                if not doublons.empty:
+                    # Suppression des doublons dans la table produits
+                    df = df.drop_duplicates()
+                    print(f"Les doublons détectés dans la table {requete["code"]} ont été supprimés")
                 resultats[requete["code"]] = df
 
                 print(f"Requête terminée. Les données de la table '{requete["code"]}' ont été récupérées.")
 
+            # Fermeture de la connexion à la bdd après récupértion des données
             connexion.close()
-            print("La récupération des données est terminées.")
-            #print(resultats)
-            donnees_approvisionnement = resultats["approvisionnement"]
-            donnees_producteurs = resultats["producteurs"]
-            donnees_produits = resultats["produits"]
+            
+            # Peuplement de 3 DataFrame avec les données des 3 tables
+            donnees_approvisionnement = pd.DataFrame(resultats["approvisionnement"])
+            donnees_producteurs = pd.DataFrame(resultats["producteurs"])
+            donnees_produits = pd.DataFrame(resultats["produits"])
 
         except sqlite.OperationalError as error:
             raise ConnectionError(f"Impossible de se connecter à la base SQLite : {error}")
 
+    print(f"Récupération des données SQLite terminée.")
     return donnees_approvisionnement,donnees_producteurs,donnees_produits
 
 if __name__ == "__main__":
